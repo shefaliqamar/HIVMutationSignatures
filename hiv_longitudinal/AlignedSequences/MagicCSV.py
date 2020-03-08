3a9309ed95e13bd808eae70a09671fa3556cfc0f
shefaliqamar <sqamar@ucsc.edu>
2020-03-08 14:57:27 -0700

    added dash skipping and opposite-mutation inclusion

--- hiv_longitudinal/AlignedSequences/MagicCSV.py
+++ hiv_longitudinal/AlignedSequences/MagicCSV.py
@@ -9,23 +9,23 @@ import shutil
 # Used to generate frequency CSVs based on reference-based comparisons!!
 # Used same reference for all studies -- may want to split by study for git file cap
 
-def generate_frequencies(divisions, mod, patientmax):
+def generate_frequencies(divisions, mod, patientmax, absmin, absmax, include):
     # -- = insert study reference
     study = "2031"
-    referenceGenome = open('../AlignedSequences/1142_21423_13_10_1986_A_1986_0__None.txt', 'r')
+    referenceGenome = open('../nucleotide2/reference_subregion_rt.txt', 'r')
 
-    path = '../AlignedSequences/'
-    
+    path = '../nucleotide2/'
 
     # convert genome to single string
     referenceSequence = ""
     with referenceGenome as reference_file:
         for line in reference_file.readlines():
             referenceSequence += line.split("\n")[0]
-    for iterations in range(0,divisions):
+    referenceSequence = referenceSequence[absmin:absmax]
+    for iterations in range(0, divisions):
         print(divisions)
         print(iterations)
-        outputFile = open("MutationDivision"+str(iterations+1)+"."+str(divisions)+".csv", "w")
+        outputFile = open("MutationDivision" + str(iterations + 1) + "." + str(divisions) + ".csv", "w")
         foundContexts = []
 
         firstPatient = True
@@ -39,67 +39,79 @@ def generate_frequencies(divisions, mod, patientmax):
             mutations["C>T"] = {}
             mutations["C>G"] = {}
             for filename in filenames:
-                if str(filename).endswith(".txt"): #and str(filename).startswith(study):
+                if str(filename).endswith(".txt"):  # and str(filename).startswith(study):
                     with open(path + filename, 'r') as test_file:
                         print("opening file", filename)
-                            #if patientnum >= 40:
-                            #return This is a limitation to the number of
+                        # if patientnum >= 40:
+                        # return This is a limitation to the number of
 
-                        if patientnum%mod==0:
+                        if patientnum % mod == 0:
                             populateContexts(mutations)
 
                         testSequence = ""
                         for line in test_file.readlines():
                             testSequence += line.split("\n")[0]
-                        substr=testSequence[round(len(testSequence)*iterations/divisions):round(len(testSequence)*iterations/divisions+len(testSequence)/divisions)]
-                        startIndex = round(len(testSequence)*iterations/divisions)
-                        endIndex = round(len(testSequence)*(iterations+1)/divisions)
-                        if "-" in substr:
+                        testSequence = testSequence[absmin:absmax]
+
+                        startIndex = len(testSequence) * iterations / divisions
+                        endIndex = len(testSequence) * (iterations + 1) / divisions
+                        if "-----" in testSequence[
+                                      startIndex:endIndex] and not include:  ##if "-" in substr and not include:
                             continue
                         patientnum += 1
-                        if patientnum>=patientmax:
-                            continue
+                        if patientnum >= patientmax:
+                            return
                         for i in range(startIndex, endIndex):
-                                # find mutations in line
-                                if testSequence[i] != referenceSequence[i]: # and testSequence[i] != "-" and referenceSequence[i] != "-":
-                                    test = randpicker(testSequence[i])
-                                    ref = randpicker(referenceSequence[i])
-                                    if test == "n" or ref == "n" or test == ref or not mutations.keys().__contains__(ref.upper() + ">" + test.upper()):
-                                        continue
-                                    # get context of test
-                                    # print("Mutation at nucleotide " + str(i) + " between: " + ref + " and: " + test)
-                                    context = ""
-                                    if i - 1 < 0:
-                                        context = "-" + ref + referenceSequence[i+1]
-                                        # print("Found a starting mutation!")
-                                    if i + 1 >= len(referenceSequence):
-                                        context = referenceSequence[i-1] + ref + "-"
-                                        # print("Found an ending mutation!")
-                                    else:
-                                        context = referenceSequence[i-1] + ref + referenceSequence[i+1]
-                                    # print("Context " + context.upper() + "of mutation at i = " + str(i))
-                                    mut = ref + ">" + test
-                                    mut = mut.upper()
-                                    context = context.upper()
-                                    if not mutations[mut].__contains__(context):
-                                        continue
-                                    else:
-                                        mutations[mut][context] = mutations[mut][context] + 1
-        
-                    #print("Mutations for file" + filename + " : " + str(mutations))
-                    if (patientnum+1)%mod==0:
+                            # find mutations in line
+                            if testSequence[i] != referenceSequence[i]:  # and testSequence[i] != "-" and referenceSequence[i] != "-":
+                                test = randpicker(testSequence[i])
+                                ref = randpicker(referenceSequence[i])
+                                # print(test)
+                                # print(test.upper())
+                                if test == "n" or ref == "n" or test == ref or not mutations.keys().__contains__(
+                                        ref.upper() + ">" + test.upper()):
+                                    continue
+
+                                # get context of test
+                                # print("Mutation at nucleotide " + str(i) + " between: " + ref + " and: " + test)
+                                context = ""
+                                if i - 1 < 0:
+                                    context = "-" + ref + referenceSequence[i + 1]
+                                    # print("Found a starting mutation!")
+                                if i + 1 >= len(referenceSequence):
+                                    context = referenceSequence[i - 1] + ref + "-"
+                                    # print("Found an ending mutation!")
+                                else:
+                                    context = referenceSequence[i - 1] + ref + referenceSequence[i + 1]
+                                # print("Context " + context.upper() + "of mutation at i = " + str(i))
+                                mut = ref + ">" + test
+                                mut = mut.upper()
+                                context = context.upper()
+                                if context.__contains__("-"):
+                                    continue
+                                if not mutations[mut].__contains__(context):
+                                    mut = opposite(mut[0]) + ">" + opposite(mut[2])
+                                    context = opposite(context[0]) + opposite(context[1]) + opposite(context[2])
+                                    mutations[mut][context] = mutations[mut][context] + 1
+
+                                else:
+                                    mutations[mut][context] = mutations[mut][context] + 1
+
+                    # print("Mutations for file" + filename + " : " + str(mutations))
+                    if (patientnum + 1) % mod == 0:
                         if firstPatient is True:
-                            with open("MutationDivision"+str(iterations+1)+"."+str(divisions)+".csv", "w") as f:
+                            with open("MutationDivision" + str(iterations + 1) + "." + str(divisions) + ".csv",
+                                      "w") as f:
                                 w = csv.writer(f)
-                                w.writerow(["Mutation Type"] + ["Trinucleotide"] + ["Sample" + str(patientnum)])
+                                w.writerow(["Mutation Type"] + ["Trinucleotide"] + ["Sample" + str(1)])
                                 for key in sorted(mutations.keys()):
                                     for context in mutations[key].keys():
                                         w.writerow([key] + [context] + [mutations[key][context]])
                                         foundContexts.append([key, context])
                             firstPatient = False
                         else:  # move on to new column for next patient
-                            filename = "MutationDivision"+str(iterations+1)+"."+str(divisions)+".csv"
-                            #print("Looking at patient number " + str(patientnum))
+                            filename = "MutationDivision" + str(iterations + 1) + "." + str(divisions) + ".csv"
+                            # print("Looking at patient number " + str(patientnum))
                             # now populate data
                             with open(filename) as csvFile:
                                 with open("dummyfile.csv", "w") as tempfile:
@@ -112,7 +124,7 @@ def generate_frequencies(divisions, mod, patientmax):
                                     for row in csv_reader:
                                         line_count += 1
                                         if line_count == 1:
-                                            csv_writer.writerow(row + ["Sample" + str(patientnum)])
+                                            csv_writer.writerow(row + ["Sample" + str((patientnum + 1) / mod)])
                                             continue
                                         if mutations[row[0]].__contains__(row[1]):
                                             csv_writer.writerow(row + [mutations[row[0]][row[1]]])
@@ -129,72 +141,73 @@ def generate_frequencies(divisions, mod, patientmax):
                                                 csv_writer.writerow(row + [mutations[key][context]])
                             shutil.move(tempfile.name, filename)
 
+
 def randpicker(letter):
     randnum = random.randint(0, 12)
-    if letter == "u":
+    if letter == "u" or letter == "U":
         return "t"
-    if letter == "t":
+    if letter == "t" or letter == "T":
         return "t"
-    if letter == "a":
+    if letter == "a" or letter == "A":
         return "a"
-    if letter == "g":
+    if letter == "g" or letter == "G":
         return "g"
-    if letter == "c":
+    if letter == "c" or letter == "C":
         return "c"
-    if letter == "n":
+    if letter == "n" or letter == "N":
         return "n"
-    if letter == "r":
+    if letter == "r" or letter == "R":
         if randnum > 6:
             return "a"
         else:
             return "g"
-    if letter == "y":
+    if letter == "y" or letter == "Y":
         if randnum > 6:
             return "c"
         else:
             return "t"
-    if letter == "s":
+    if letter == "s" or letter == "S":
         if randnum > 6:
             return "g"
         else:
             return "c"
-    if letter == "w":
+    if letter == "w" or letter == "W":
         if randnum > 6:
             return "a"
         else:
             return "t"
-    if letter == "k":
+    if letter == "k" or letter == "K":
         if randnum > 6:
             return "g"
         else:
             return "t"
-    if letter == "m":
+    if letter == "m" or letter == "M":
         if randnum > 6:
             return "a"
         else:
             return "c"
-    if letter == "b":
+    if letter == "b" or letter == "B":
         if randnum > 4:
             return "c"
         if randnum > 8:
             return "g"
         else:
             return "t"
-    if letter == "d":
+    if letter == "d" or letter == "D":
         if randnum > 4:
             return "a"
         if randnum > 8:
             return "g"
         else:
             return "t"
-    if letter == "h":
+    if letter == "h" or letter == "H":
         if randnum > 4:
             return "a"
         if randnum > 8:
             return "c"
         else:
             return "t"
-    if letter == "v":
+    if letter == "v" or letter == "V":
         if randnum > 4:
             return "a"
         if randnum > 8:
@@ -203,7 +216,22 @@ def randpicker(letter):
             return "g"
 
     else:
-        return "illegal value exception"
+        return letter
+
+
+def opposite(char):
+    char = char.lower()
+    if char == "A":
+        return "T"
+    if char == "T":
+        return "A"
+    if char == "C":
+        return "G"
+    if char == "G":
+        return "C"
+    if char == ">":
+        return ">"
+
 
 def populateContexts(mutations):
     left = "A"
@@ -230,5 +258,6 @@ def populateContexts(mutations):
                 context = left + key[0] + right
                 mutations[key][context] = 0
 
-generate_frequencies(10,10,1000)
+
+generate_frequencies(1, 50, 7000, 2, 2000, False)
 # To do
